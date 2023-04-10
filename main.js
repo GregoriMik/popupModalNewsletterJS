9fad3a964f2e200b202e554897c598e993493ddf
Grzegorz Mikina <gregorimikina@gmail.com>
2023-04-10 22:12:05 +0200

    Rename page title, add fixed position to content

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // alert('hello');
+const newsletter = document.querySelector('.newsletter-modal');
+const newForm = document.getElementById('news-form');
+
+window.addEventListener('load',()=>{
+    setTimeout(()=>{
+        newsletter.style.display="block";
+    },7000);
+})
